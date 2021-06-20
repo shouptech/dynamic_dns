8b6c250f1eac9a9d79148c3e8e969669c1e0e581
Mike Shoup <mike@shoup.io>
2021-06-20 04:21:10 -0600

    Reformat, and use python3

--- cloudflare_dynamicdns.py
+++ cloudflare_dynamicdns.py
@@ -1,89 +1,81 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import requests
 
+
 def get_ip(ipurl):
-    """ Retrieves current IP """
+    """Retrieves current IP"""
     response = requests.get(ipurl)
     return response.text.strip()
 
+
 def get_zone_id(dnsname, authemail, authkey):
-    """ Return zone id """
-    headers = {
-        'X-Auth-Email': authemail,
-        'X-Auth-Key': authkey
-    }
-    params = {
-        'name': "%s.%s" % tuple(dnsname.split('.')[-2:])
-    }
-    url = 'https://api.cloudflare.com/client/v4/zones'
+    """Return zone id"""
+    headers = {"X-Auth-Email": authemail, "X-Auth-Key": authkey}
+    params = {"name": "%s.%s" % tuple(dnsname.split(".")[-2:])}
+    url = "https://api.cloudflare.com/client/v4/zones"
     response = requests.get(url, headers=headers, params=params)
-    return response.json()['result'][0]['id']
+    return response.json()["result"][0]["id"]
+
 
 def get_dns_record(zoneid, dnsname, authemail, authkey):
-    """ Get DNS record address and id """
-    headers = {
-        'X-Auth-Email': authemail,
-        'X-Auth-Key': authkey
-    }
-    params = {
-        'name': dnsname,
-        'type': 'A'
-    }
+    """Get DNS record address and id"""
+    headers = {"X-Auth-Email": authemail, "X-Auth-Key": authkey}
+    params = {"name": dnsname, "type": "A"}
     url = "https://api.cloudflare.com/client/v4/zones/%s/dns_records" % zoneid
     response = requests.get(url, headers=headers, params=params)
 
-    if response.json()['result_info']['total_count'] > 0:
+    if response.json()["result_info"]["total_count"] > 0:
         return {
-            'id': response.json()['result'][0]['id'],
-            'address': response.json()['result'][0]['content']
+            "id": response.json()["result"][0]["id"],
+            "address": response.json()["result"][0]["content"],
         }
     return None
 
+
 def create_dns(zoneid, dnsname, ip, authemail, authkey):
-    """ Create DNS record """
-    headers = {
-        'X-Auth-Email': authemail,
-        'X-Auth-Key': authkey
-    }
+    """Create DNS record"""
+    headers = {"X-Auth-Email": authemail, "X-Auth-Key": authkey}
     payload = {
-        'type': 'A',
-        'name': dnsname,
-        'content': ip,
-        'ttl': 120,
-        'proxied': False
+        "type": "A",
+        "name": dnsname,
+        "content": ip,
+        "ttl": 120,
+        "proxied": False,
     }
     url = "https://api.cloudflare.com/client/v4/zones/%s/dns_records" % zoneid
     response = requests.post(url, headers=headers, json=payload)
     return response
 
+
 def update_dns(zoneid, recordid, dnsname, ip, authemail, authkey):
-    """ Update DNS record """
-    headers = {
-        'X-Auth-Email': authemail,
-        'X-Auth-Key': authkey
-    }
-    payload = {
-        'type': 'A',
-        'name': dnsname,
-        'content': ip
-    }
+    """Update DNS record"""
+    headers = {"X-Auth-Email": authemail, "X-Auth-Key": authkey}
+    payload = {"type": "A", "name": dnsname, "content": ip}
     url = "https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s" % (
-        zoneid, recordid)
+        zoneid,
+        recordid,
+    )
     response = requests.put(url, headers=headers, json=payload)
     return response
 
+
 def main():
-    """ Parse arguments and update IP as needed """
+    """Parse arguments and update IP as needed"""
 
     parser = argparse.ArgumentParser(
-        description='Update DNS record in cloudflare to current IP')
-    parser.add_argument('-i', '--ipurl', default='http://ifconfig.me/ip',
-        help='URL to query for current IP address')
-    parser.add_argument('dnsname', help='DNS Name to update')
-    parser.add_argument('authemail', help='Cloudflare Auth Email')
-    parser.add_argument('authkey', help='Cloudflare Auth Key')
+        description="Update DNS record in cloudflare to current IP"
+    )
+    parser.add_argument(
+        "-i",
+        "--ipurl",
+        default="http://ifconfig.me/ip",
+        help="URL to query for current IP address",
+    )
+    parser.add_argument("dnsname", help="DNS Name to update")
+    parser.add_argument("authemail", help="Cloudflare Auth Email")
+    parser.add_argument("authkey", help="Cloudflare Auth Key")
     args = parser.parse_args()
 
     zoneid = get_zone_id(args.dnsname, args.authemail, args.authkey)
@@ -94,20 +86,21 @@ def main():
     if curdns == None:
         print("DNS record does not exist. Creating record:")
         print("%s - %s" % (args.dnsname, myip))
-        response = create_dns(
-            zoneid, args.dnsname, myip, args.authemail, args.authkey)
+        response = create_dns(zoneid, args.dnsname, myip, args.authemail, args.authkey)
         print(response.json())
 
-    elif myip == curdns['address']:
-        print("IPs match: %s == %s, doing nothing" % (myip, curdns['address']))
+    elif myip == curdns["address"]:
+        print("IPs match: %s == %s, doing nothing" % (myip, curdns["address"]))
 
     else:
         print("DNS record does not match current IP. Updating record:")
-        print("Old value: %s - %s" % (args.dnsname, curdns['address']))
+        print("Old value: %s - %s" % (args.dnsname, curdns["address"]))
         print("New value: %s - %s" % (args.dnsname, myip))
-        response = update_dns(zoneid,
-            curdns['id'], args.dnsname, myip, args.authemail, args.authkey)
+        response = update_dns(
+            zoneid, curdns["id"], args.dnsname, myip, args.authemail, args.authkey
+        )
         print(response.json())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
